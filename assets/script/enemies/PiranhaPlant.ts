a9e0f47c1104773e8ada7f3bef7d61991ffe4907
thanhtu <33388271+hugomaiket360@users.noreply.github.com>
2024-03-12 16:09:28 +0700

    update 5

--- assets/script/enemies/PiranhaPlant.ts
+++ assets/script/enemies/PiranhaPlant.ts
@@ -13,18 +13,21 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class PiranhaPlant extends cc.Component {
     piranhaPlant: cc.Node = null;
+    piranhaPlantTemp: cc.Node = null;
     actionPlant : cc.Tween<cc.Node> = null;
 
     protected onLoad(): void {
-        this.piranhaPlant = this.node.getChildByName("piranha_plant");
+        this.piranhaPlantTemp = this.node.getChildByName("mask");
+        this.piranhaPlant = this.piranhaPlantTemp.getChildByName("piranha_plant");
+        console.log(this.piranhaPlant.name);
     }
 
     onCollisionEnter (other: cc.BoxCollider, self: cc.BoxCollider) {
         if(other.tag == ENUM_COLLIDER_TAG.PLAYER && self.tag == ENUM_COLLIDER_TAG.PLANT_VIEW){
             this.actionPlant = cc.tween(this.piranhaPlant).sequence(
-                cc.tween(this.piranhaPlant).to(0.25,{y: 40}),
+                cc.tween(this.piranhaPlant).to(0.25,{y: 0}),
                 cc.tween().delay(0.25),
-                cc.tween(this.piranhaPlant).to(0.5,{y: 0}),
+                cc.tween(this.piranhaPlant).to(0.5,{y: -84}),
                 cc.tween().delay(1.5),
             )
             .repeat(20)
