5ae3a881e202b74b0170c5964adb59da26dbd99a
HarrisTran <dangkhoat81@gmail.com>
2024-06-27 14:50:05 +0700

    trigger play audio

--- assets/_Game/Script/enemies/StandTrap.ts
+++ assets/_Game/Script/enemies/StandTrap.ts
@@ -1,5 +1,6 @@
 
-import { ENUM_COLLIDER_TAG } from "../Enum";
+import { ENUM_AUDIO_CLIP, ENUM_COLLIDER_TAG } from "../Enum";
+import GameManager from "../manager/GameManager";
 
 const {ccclass, property} = cc._decorator;
 
@@ -12,6 +13,7 @@ export default class StandTrap extends cc.Component {
     onCollisionEnter (other: cc.BoxCollider, self: cc.BoxCollider) {
         if(other.tag == ENUM_COLLIDER_TAG.PLAYER && self.tag == ENUM_COLLIDER_TAG.SPIKE){
             this.trapAnimation.setAnimation(0,'attack',false);
+            GameManager.Instance.audioManager.playSfx(ENUM_AUDIO_CLIP.TRAP_STAND);
         }
     }
 
